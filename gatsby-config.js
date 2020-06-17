6d77759340ba1f3e3fb0b59f19f04e558d747156
Nathan Kleekamp <nkleekamp@gmail.com>
2020-06-17 18:35:56 -0400

    prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,88 +1,88 @@
-const title = 'Big Bright Pixels';
+const title = "Big Bright Pixels";
 
 module.exports = {
   siteMetadata: {
     title,
-    author: 'Nathan Kleekamp',
-    description: 'Thoughts on code.',
-    siteUrl: 'https://bigbrightpixels.com',
+    author: "Nathan Kleekamp",
+    description: "Thoughts on code.",
+    siteUrl: "https://bigbrightpixels.com",
     social: {
-      twitter: 'nathankleekamp',
-      github: 'nathankleekamp',
+      twitter: "nathankleekamp",
+      github: "nathankleekamp",
     },
   },
   plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content/blog`,
-        name: 'blog',
+        name: "blog",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content/assets`,
-        name: 'assets',
+        name: "assets",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content/glossary`,
-        name: 'glossary',
+        name: "glossary",
       },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 590,
             },
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
+            resolve: "gatsby-remark-responsive-iframe",
             options: {
-              wrapperStyle: 'margin-bottom: 1.0725rem',
+              wrapperStyle: "margin-bottom: 1.0725rem",
             },
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-          'gatsby-remark-widows',
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
+          "gatsby-remark-widows",
         ],
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-catch-links',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-catch-links",
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: 'UA-149363325-1',
+        trackingId: "UA-149363325-1",
       },
     },
-    'gatsby-plugin-feed',
+    "gatsby-plugin-feed",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
         name: title,
         short_name: title,
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#663399',
-        display: 'minimal-ui',
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#663399",
+        display: "minimal-ui",
       },
     },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-offline",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-plugin-typography',
+      resolve: "gatsby-plugin-typography",
       options: {
-        pathToConfigModule: 'src/utils/typography',
+        pathToConfigModule: "src/utils/typography",
       },
     },
   ],
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,7 @@
-const path = require('path');
-const { createFilePath } = require('gatsby-source-filesystem');
+const path = require("path");
+const { createFilePath } = require("gatsby-source-filesystem");
 
-const constants = {
-  GLOSSARY: 'glossary',
-};
+const constants = { GLOSSARY: "glossary" };
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
@@ -25,25 +23,31 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
-    }`
-  );
+    }
+  `);
 
   if (result.errors) {
     throw result.errors;
   }
 
-  const { posts, glossaryTerms } = result.data.allMarkdownRemark.edges.reduce((accum, current) => {
-    if ((((current || {}).node || {}).frontmatter || {}).postType === constants.GLOSSARY) {
-      accum.glossaryTerms.push(current);
-      return accum;
-    }
+  const { posts, glossaryTerms } = result.data.allMarkdownRemark.edges.reduce(
+    (accum, current) => {
+      if (
+        (((current || {}).node || {}).frontmatter || {}).postType ===
+        constants.GLOSSARY
+      ) {
+        accum.glossaryTerms.push(current);
+        return accum;
+      }
 
-    accum.posts.push(current);
-    return accum;
-  }, {
-    posts: [],
-    glossaryTerms: [],
-  });
+      accum.posts.push(current);
+      return accum;
+    },
+    {
+      posts: [],
+      glossaryTerms: [],
+    }
+  );
 
   // Create individual blog posts pages with next/previous navigation within posts
   posts.forEach((post, index) => {
@@ -51,7 +55,7 @@ exports.createPages = async ({ graphql, actions }) => {
     const next = index === 0 ? null : posts[index - 1].node;
     createPage({
       path: post.node.fields.slug,
-      component: path.resolve('./src/templates/blog-post.js'),
+      component: path.resolve("./src/templates/blog-post.js"),
       context: {
         slug: post.node.fields.slug,
         previous,
@@ -61,13 +65,13 @@ exports.createPages = async ({ graphql, actions }) => {
   });
 
   // Create individual glossary term pages
-  glossaryTerms.forEach(term => {
+  glossaryTerms.forEach((term) => {
     const slug = term.node.fields.slug;
     const postPath = `/glossary${slug}`;
     createPage({
       path: postPath,
-      component: path.resolve('./src/templates/glossary-term.js'),
-      context: { slug }
+      component: path.resolve("./src/templates/glossary-term.js"),
+      context: { slug },
     });
   });
 
@@ -76,8 +80,8 @@ exports.createPages = async ({ graphql, actions }) => {
   const numPages = Math.ceil(posts.length / postsPerPage);
   Array.from({ length: numPages }).forEach((_, i) => {
     createPage({
-      path: i === 0 ? '/' : `/blog/${i + 1}`,
-      component: path.resolve('./src/templates/blog-list.js'),
+      path: i === 0 ? "/" : `/blog/${i + 1}`,
+      component: path.resolve("./src/templates/blog-list.js"),
       context: {
         limit: postsPerPage,
         skip: i * postsPerPage,
@@ -91,10 +95,10 @@ exports.createPages = async ({ graphql, actions }) => {
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
 
-  if (node.internal.type === 'MarkdownRemark') {
+  if (node.internal.type === "MarkdownRemark") {
     const value = createFilePath({ node, getNode });
     createNodeField({
-      name: 'slug',
+      name: "slug",
       node,
       value,
     });
