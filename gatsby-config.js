9bc014e2f35000e0b62bb0601c74be5016dce9a4
Nathan Kleekamp <nkleekamp@gmail.com>
2021-07-18 16:46:54 -0400

    upgrade project deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,7 @@ module.exports = {
         background_color: "#ffffff",
         theme_color: "#663399",
         display: "minimal-ui",
+        icon: "content/assets/bbp-logo.png",
       },
     },
     "gatsby-plugin-offline",
